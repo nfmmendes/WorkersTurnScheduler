8e7bd913d049745b2b203ce3f1b4b7ec1988f1e5
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-06 11:56:55 +0200

    Create workers repository

---
+++ WorkersTurnScheduler/Services/IWorkerRepository.cs
@@ -0,0 +1,9 @@
+﻿using WorkersTurnScheduler.Domain;
+
+namespace WorkersTurnScheduler.Services
+{
+    public interface IWorkerRepository
+    {
+        List<Worker> GetAllWorkers(); 
+    }
+}
---
+++ WorkersTurnScheduler/Services/WorkerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkersTurnScheduler.Domain;
+
+namespace WorkersTurnScheduler.Services
+{
+    public class WorkerRepository : IWorkerRepository
+    {
+        private List<Worker> Workers 
+        {
+            get => new List<Worker>{
+                                                    new Worker("Paulo", "Pessoa"),
+                                                    new Worker("Carlos", "Silva"),
+                                                    new Worker("Alan", "Carvalho"),
+                                                    new Worker("Rodrigo", "Ribeiro") };
+        }
+
+        public List<Worker> GetAllWorkers()
+        {
+            return Workers;
+        }
+    }
+}
