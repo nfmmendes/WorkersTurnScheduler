145121c974a0320b054d1ccfb52a6cb5cbabbca4
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-06 17:17:45 +0200

    Create classes to represent worker availability during the week

---
+++ WorkersTurnScheduler/Domain/AvailabilitySlot.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkersTurnScheduler.Domain
+{
+    public class AvailabilitySlot
+    {
+        [Required]
+        List<DayOfWeek> WeekDays { get; set; }
+
+        [Required]
+        [Range(1, 53)]
+        int InitialWeekIndex { get; set; }
+
+        [Range(1, 53)]
+        int FinalWeekIndex { get; set; }
+
+        [Required]
+        public TimeOnly Start { get; set; }
+
+        [Required]
+        public TimeOnly End { get; set; }
+
+        private AvailabilitySlot() { }
+    }
+}
---
+++ WorkersTurnScheduler/Domain/WorkerAvailabilitySlots.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkersTurnScheduler.Domain
+{
+    public class WorkerAvailabilitySlots
+    {
+        [Key]
+        public UInt128 WorkerId { get; private set; } 
+
+        public List<AvailabilitySlot> AvailabilitySlots { get; private set; }
+
+        private WorkerAvailabilitySlots() { }
+    }
+}
