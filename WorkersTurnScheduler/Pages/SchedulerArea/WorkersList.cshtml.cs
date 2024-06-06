60cc122b7f539ba750f6149dac2ce84ddd94a385
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-06 12:00:56 +0200

    Refactor WorkersList page model

--- WorkersTurnScheduler/Pages/SchedulerArea/WorkersList.cshtml.cs
+++ WorkersTurnScheduler/Pages/SchedulerArea/WorkersList.cshtml.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WorkersTurnScheduler.Domain;
+using WorkersTurnScheduler.Services;
 
 namespace WorkersTurnScheduler.Pages.SchedulerArea
 {
     public class WorkersListModel : PageModel
     {
-        public List<Worker> Workers { get => new List<Worker>{ 
-                                                    new Worker("Paulo", "Pessoa"),
-                                                    new Worker("Carlos", "Silva"), 
-                                                    new Worker("Alan", "Carvalho"), 
-                                                    new Worker("Rodrigo", "Ribeiro") }; }
+        public List<Worker> Workers { get; private set; }
 
+        public IWorkerRepository _repository { get; private set; }
+
+        public WorkersListModel(IWorkerRepository repository)
+        {
+            _repository = repository;
+            Workers = new List<Worker>();
+        }
+        
         public void OnGet()
         {
+            Workers = _repository.GetAllWorkers();
         }
     }
 }
