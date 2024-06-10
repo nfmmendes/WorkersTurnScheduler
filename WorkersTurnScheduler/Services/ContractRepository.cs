1e25f80b9596e9704d07b136e2a8f3c28fbb3303
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-10 11:40:20 +0200

    Create Contract repository

---
+++ WorkersTurnScheduler/Services/ContractRepository.cs
@@ -0,0 +1,23 @@
+﻿using WorkersTurnScheduler.Domain;
+
+namespace WorkersTurnScheduler.Services
+{
+    public class ContractRepository : IContractRepository
+    {
+        IWorkerRepository _workerRepository;
+        public ContractRepository(IWorkerRepository workerRepository) { 
+            _workerRepository = workerRepository;
+        }
+
+        public Contract? getContract(UInt128 contractId)
+        {
+            // TODO: Search it directly on Contract list when it will be available.
+            var worker = _workerRepository.GetAllWorkers().FirstOrDefault(worker => worker.Contract.Id == contractId);
+
+            if (worker != null)
+                return worker.Contract;
+            return null;
+        }
+
+    }
+}
