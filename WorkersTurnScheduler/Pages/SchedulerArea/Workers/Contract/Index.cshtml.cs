a55ca4370b177b99555b997588adb6a32d91f199
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-24 15:34:18 +0200

    Document contract index page

--- WorkersTurnScheduler/Pages/SchedulerArea/Workers/Contract/Index.cshtml.cs
+++ WorkersTurnScheduler/Pages/SchedulerArea/Workers/Contract/Index.cshtml.cs
@@ -6,19 +6,43 @@ using WorkersTurnScheduler.Services;
 
 namespace WorkersTurnScheduler.Pages.SchedulerArea.Workers.Contract
 {
+    /// <summary>
+    /// Class <c>ContractModel</c> is a page model class that contains data and methods 
+    /// needed to manage and render contract index page. 
+    /// </summary>
+    /// <param name="workerRepository">Worker repository passed as injected depedency </param>
     public class ContractModel(IWorkerRepository workerRepository) : PageModel
     {
+        /// <value>
+        /// Worker repository.
+        /// </value>
         private IWorkerRepository _repository { get; set; } = workerRepository;
 
+        /// <value>
+        /// DTO containing currenct contract data. 
+        /// </value>
         public Domain.Contract? Contract { get; private set; }
 
+        /// <value>
+        /// Worker name. 
+        /// </value>
         public string WorkerName { get; private set; } = "";
 
+        /// <value>
+        /// Worker surname.
+        /// </value>
         public string WorkerSurname { get; private set; } = "";
 
+        /// <value>
+        /// Worker id.
+        /// </value>
         [BindProperty]
         public UInt128 WorkerId { get; set; }
 
+        /// <summary>
+        /// The page GET function. 
+        /// </summary>
+        /// <returns>A rendered page if everything is ok. An error page otherwise. </returns>
         public IActionResult OnGet()
         {
             HttpContext.Request.RouteValues.TryGetValue("workerId", out object workerIdObject);
@@ -44,6 +68,10 @@ namespace WorkersTurnScheduler.Pages.SchedulerArea.Workers.Contract
             return Page();
         }
 
+        /// <summary>
+        /// The page error function. 
+        /// </summary>
+        /// <returns></returns>
         private IActionResult Error()
         {
             return RedirectToPage("./../../Error");
