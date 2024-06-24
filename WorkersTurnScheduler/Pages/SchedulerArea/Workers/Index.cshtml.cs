24496e258312d9453d9443fc33b04dbff599ec00
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-24 15:50:20 +0200

    Document worker list page model.

--- WorkersTurnScheduler/Pages/SchedulerArea/Workers/Index.cshtml.cs
+++ WorkersTurnScheduler/Pages/SchedulerArea/Workers/Index.cshtml.cs
@@ -5,18 +5,35 @@ using WorkersTurnScheduler.Services;
 
 namespace WorkersTurnScheduler.Pages.SchedulerArea.Workers
 {
+    /// <summary>
+    /// Class <c>WorkersListModel</c> contains data and functions needed to render and 
+    /// manage the workers list page. 
+    /// </summary>
     public class WorkersListModel : PageModel
     {
+        /// <value>
+        /// The workers list. 
+        /// </value>
         public List<Worker> Workers { get; private set; }
 
+        /// <value>
+        /// Worker repository
+        /// </value>
         public IWorkerRepository _repository { get; private set; }
 
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="repository">Worker repository passed as injected dependency</param>
         public WorkersListModel(IWorkerRepository repository)
         {
             _repository = repository;
             Workers = new List<Worker>();
         }
 
+        /// <summary>
+        /// Page get function.
+        /// </summary>
         public void OnGet()
         {
             Workers = _repository.GetAllWorkers();
