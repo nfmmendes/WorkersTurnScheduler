f1440366f789ec8b1833186b8666ba364f40d946
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-24 13:09:25 +0200

    Add documentation to AvailabilitySlot

--- WorkersTurnScheduler/Domain/AvailabilitySlot.cs
+++ WorkersTurnScheduler/Domain/AvailabilitySlot.cs
@@ -2,21 +2,40 @@
 
 namespace WorkersTurnScheduler.Domain
 {
+    /// <summary>
+    /// Class <c> AvailabilitySlot </c> models the worker time slot availability during a 
+    /// set of days and weeks. 
+    /// </summary>
     public class AvailabilitySlot
     {
+        /// <value>
+        /// The week days in which the worker is available. 
+        /// </value>
         [Required]
         List<DayOfWeek> WeekDays { get; set; }
 
+        /// <value>
+        /// The availability slot first week index.
+        /// </value>
         [Required]
         [Range(1, 53)]
         int InitialWeekIndex { get; set; }
 
+        /// <value>
+        /// The availability slot last week index.
+        /// </value>
         [Range(1, 53)]
         int FinalWeekIndex { get; set; }
 
+        /// <value>
+        /// The slot start time.
+        /// </value>
         [Required]
         public TimeOnly Start { get; set; }
 
+        /// <value>
+        /// The slot end time
+        /// </value>
         [Required]
         public TimeOnly End { get; set; }
 
