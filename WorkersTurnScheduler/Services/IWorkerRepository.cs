f268de90902ce9ed729e7cc302a906b785e9caa7
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 11:58:27 +0200

    Add documentation on worker repository interface

--- WorkersTurnScheduler/Services/IWorkerRepository.cs
+++ WorkersTurnScheduler/Services/IWorkerRepository.cs
@@ -2,10 +2,22 @@
 
 namespace WorkersTurnScheduler.Services
 {
+    /// <summary>
+    /// WorkerRepository interface.
+    /// </summary>
     public interface IWorkerRepository
     {
+        /// <summary>
+        /// Get a list with all the workers. 
+        /// </summary>
+        /// <returns>A list of workers. </returns>
         List<Worker> GetAllWorkers(); 
 
+        /// <summary>
+        /// Get a worker. 
+        /// </summary>
+        /// <param name="id">The worker id.</param>
+        /// <returns>A worker. </returns>
         Worker GetWorker(UInt128 id);
     }
 }
