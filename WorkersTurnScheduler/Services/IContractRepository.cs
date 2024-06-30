3732d9c9e137566df5dfa26dfa1c9f16a7e30cc7
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 11:49:07 +0200

    Add documentation on IContractRepository

--- WorkersTurnScheduler/Services/IContractRepository.cs
+++ WorkersTurnScheduler/Services/IContractRepository.cs
@@ -2,8 +2,16 @@
 
 namespace WorkersTurnScheduler.Services
 {
+    /// <summary>
+    /// ContractRepository inteface
+    /// </summary>
     public interface IContractRepository
     {
+        /// <summary>
+        /// Get the contract by contract id.
+        /// </summary>
+        /// <param name="contractId">The contract id. </param>
+        /// <returns> Return a contract, if the contract exists, null otherwise.</returns>
         public Contract? getContract(UInt128 contractId);
     }
 }
