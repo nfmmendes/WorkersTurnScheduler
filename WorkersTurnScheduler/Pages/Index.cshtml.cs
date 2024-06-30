f468420e05c669ef2fe59ebcdb8f4e9967b838cc
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 12:31:32 +0200

    Add documentation on index page.

--- WorkersTurnScheduler/Pages/Index.cshtml.cs
+++ WorkersTurnScheduler/Pages/Index.cshtml.cs
@@ -3,15 +3,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkersTurnScheduler.Pages
 {
+    /// <summary>
+    /// Class <c>IndexModel</c> contains the data and structure to handle the initial page.
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <value>
+        /// The logger object.
+        /// </value>
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="logger"> Logger passed as injected dependence. </param>
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// The get fucntion.
+        /// </summary>
         public void OnGet()
         {
 
