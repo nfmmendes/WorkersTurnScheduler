47f7f1ac87cbab564337255cb02f7e6a0e65541e
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 12:40:06 +0200

    Add documentation on Privacy model class.

--- WorkersTurnScheduler/Pages/Privacy.cshtml.cs
+++ WorkersTurnScheduler/Pages/Privacy.cshtml.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkersTurnScheduler.Pages
 {
+    /// <summary>
+    /// Class <c>PrivateModel</c> includes the data and functions to 
+    /// manage the privacy page.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        /// <value>
+        /// The logger object.
+        /// </value>
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="logger">The log object.</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
