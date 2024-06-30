12266204963eee25531da093ef7283cee1791289
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 12:51:29 +0200

    Include  documentation on ErrorPage

--- WorkersTurnScheduler/Pages/Error.cshtml.cs
+++ WorkersTurnScheduler/Pages/Error.cshtml.cs
@@ -4,25 +4,43 @@ using System.Diagnostics;
 
 namespace WorkersTurnScheduler.Pages
 {
+    /// <summary>
+    /// Class <c>ErrorModel</c> contains data and functions to handle the error page
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
+        /// <value>
+        /// The request id. 
+        /// </value>
         public string? RequestId { get; set; }
 
+        /// <summary>
+        /// Get if the request id must be shown.
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        /// <value>
+        /// Logger object.
+        /// </value>
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="logger">The logger object.</param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Page get function.
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
-
 }
