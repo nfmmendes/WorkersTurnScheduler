a0d5d6194a56016e1e7059b35ace477badd37862
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 13:05:42 +0200

    Add documentation to worker availability slots.

--- WorkersTurnScheduler/Domain/WorkerAvailabilitySlots.cs
+++ WorkersTurnScheduler/Domain/WorkerAvailabilitySlots.cs
@@ -2,13 +2,26 @@
 
 namespace WorkersTurnScheduler.Domain
 {
+    /// <summary>
+    /// Class <c>WorkerAvailabilitySlots</c> contains the data to represent
+    /// the connection between workers and their availability slots. 
+    /// </summary>
     public class WorkerAvailabilitySlots
     {
+        /// <value>
+        /// Worker id.
+        /// </value>
         [Key]
         public UInt128 WorkerId { get; private set; } 
 
+        /// <summary>
+        /// The list of availability slots to the given worker. 
+        /// </summary>
         public List<AvailabilitySlot> AvailabilitySlots { get; private set; }
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         private WorkerAvailabilitySlots() { }
     }
 }
