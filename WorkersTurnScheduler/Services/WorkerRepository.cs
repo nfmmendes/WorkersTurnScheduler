98f52825d28ff2cd90efc5446103a21e71e2c95c
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 12:28:11 +0200

    Correct identation

--- WorkersTurnScheduler/Services/WorkerRepository.cs
+++ WorkersTurnScheduler/Services/WorkerRepository.cs
@@ -19,10 +19,10 @@ namespace WorkersTurnScheduler.Services
                 Worker.ResetId();
                 Contract.ResetId();
                 return new List<Worker>{
-                                                    new Worker("Paulo", "Pessoa", new Contract(ContractType.Regular, new Tuple<int, int>(20, 40), new Tuple<int, int>(3, 5), new Tuple<int, int>(3, 8) )),
-                                                    new Worker("Carlos", "Silva", new Contract(ContractType.Regular, new Tuple<int, int>(30, 40), new Tuple<int, int>(4, 5), new Tuple<int, int>(6, 8) )),
-                                                    new Worker("Alan", "Carvalho",new Contract(ContractType.Freelance, new Tuple<int, int>(20, 40), new Tuple<int, int>(1, 3), new Tuple<int, int>(4, 6) ) ),
-                                                    new Worker("Rodrigo", "Ribeiro", new Contract(ContractType.Temporary, new Tuple<int, int>(40, 40), new Tuple<int, int>(5, 5), new Tuple<int, int>(8, 8) )) };
+                    new Worker("Paulo", "Pessoa", new Contract(ContractType.Regular, new Tuple<int, int>(20, 40), new Tuple<int, int>(3, 5), new Tuple<int, int>(3, 8) )),
+                    new Worker("Carlos", "Silva", new Contract(ContractType.Regular, new Tuple<int, int>(30, 40), new Tuple<int, int>(4, 5), new Tuple<int, int>(6, 8) )),
+                    new Worker("Alan", "Carvalho",new Contract(ContractType.Freelance, new Tuple<int, int>(20, 40), new Tuple<int, int>(1, 3), new Tuple<int, int>(4, 6) ) ),
+                    new Worker("Rodrigo", "Ribeiro", new Contract(ContractType.Temporary, new Tuple<int, int>(40, 40), new Tuple<int, int>(5, 5), new Tuple<int, int>(8, 8) )) };
             }
         }
 
