565430be17731ef97dbdf70b9e30b20fe441d78b
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 11:46:04 +0200

    Add documentation on ContractRepository

--- WorkersTurnScheduler/Services/ContractRepository.cs
+++ WorkersTurnScheduler/Services/ContractRepository.cs
@@ -2,13 +2,30 @@
 
 namespace WorkersTurnScheduler.Services
 {
+    /// <summary>
+    /// Class <c>ContractRepository</c> holds the functions needed to
+    /// access contract data on database. 
+    /// </summary>
     public class ContractRepository : IContractRepository
     {
+        /// <value>
+        /// Workers repository
+        /// </value>
         IWorkerRepository _workerRepository;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="workerRepository"> Worker repository passed as injected dependence. </param>
         public ContractRepository(IWorkerRepository workerRepository) { 
             _workerRepository = workerRepository;
         }
 
+       /// <summary>
+       /// Get the contract by contract id.
+       /// </summary>
+       /// <param name="contractId">The contract id. </param>
+       /// <returns> Return a contract, if the contract exists, null otherwise.</returns>
         public Contract? getContract(UInt128 contractId)
         {
             // TODO: Search it directly on Contract list when it will be available.
