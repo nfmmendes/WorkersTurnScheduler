4f527e1f61976aed89a0eeeabbdf56a0c12640ea
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 12:41:14 +0200

    Add documentation on IndexWithTextModel

--- WorkersTurnScheduler/Pages/IndexWithText.cshtml.cs
+++ WorkersTurnScheduler/Pages/IndexWithText.cshtml.cs
@@ -3,8 +3,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkersTurnScheduler.Pages
 {
+    /// <summary>
+    /// Class <c>IndexWithTextModel</c> contains data and functions to 
+    /// manage the index page with a default text. 
+    /// </summary>
     public class IndexWithTextModel : PageModel
     {
+        /// <summary>
+        /// Page get function. 
+        /// </summary>
         public void OnGet()
         {
             ViewData["Message"] = "Hello world";
