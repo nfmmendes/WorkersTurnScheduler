fadbba266d5dc25709262fb01f43643a2b8c2882
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-30 11:28:30 +0200

    Include documentation on HomeModel

--- WorkersTurnScheduler/Pages/SchedulerArea/Home.cshtml.cs
+++ WorkersTurnScheduler/Pages/SchedulerArea/Home.cshtml.cs
@@ -3,12 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WorkersTurnScheduler.Pages.SchedulerArea
 {
+    /// <summary>
+    /// Class <c>HomeModel</c> contains data and functions needed to render and 
+    /// manage the home page
+    /// </summary>
     public class HomeModel : PageModel
     {
+        /// <value>
+        /// User name field. 
+        /// </value>
         private string userName = "user" ;
 
+        /// <value>
+        /// User name property.
+        /// </value>
         public string UserName { get => userName; private set => userName = value; }
 
+        /// <summary>
+        /// Page get function.
+        /// </summary>
         public void OnGet()
         {
             UserName = "Pedro Pedroso";
