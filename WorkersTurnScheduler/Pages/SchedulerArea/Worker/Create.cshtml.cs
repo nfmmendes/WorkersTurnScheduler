6915cb85b76744f4ef05362c04eb8e1c780a74d4
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-15 12:02:49 +0200

    Include documentation

--- WorkersTurnScheduler/Pages/SchedulerArea/Worker/Create.cshtml.cs
+++ WorkersTurnScheduler/Pages/SchedulerArea/Worker/Create.cshtml.cs
@@ -4,15 +4,30 @@ using WorkersTurnScheduler.Services;
 
 namespace WorkersTurnScheduler.Pages.SchedulerArea.Worker
 {
+    /// <summary>
+    /// Class <c>CreateModel</c> is a page model class that
+    /// holds that and methods to render the worker create page. 
+    /// </summary>
     public class CreateModel : PageModel
     {
+        /// <value>
+        /// Worker's repository.
+        /// </value>
         IWorkerRepository _repository;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="repository">Worker repository, injected as a dependence.</param>
         public CreateModel(IWorkerRepository repository)
         {
             _repository = repository;
         }
 
+        /// <summary>
+        /// Page get function.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnGet()
         {
             return Page();
