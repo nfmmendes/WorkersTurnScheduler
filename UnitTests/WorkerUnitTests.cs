10f0647636cc6ff474f731e2250c51e785e1c5aa
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-31 11:13:02 +0200

    Add class to test worker class

---
+++ UnitTests/WorkerUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WorkersTurnScheduler.Domain;
+
+namespace UnitTests;
+
+public class WorkerUnitTests
+{
+    [Fact]
+    public void TestSetName()
+    {
+        Worker worker = new Worker();
+        string newName = "New name";
+
+        worker.Name = newName;
+        Assert.Equal(worker.Name, newName);
+    }
+
+    [Fact]
+    public void TestSetSurname()
+    {
+        Worker worker = new Worker();
+        string newSurname = "Newsurname";
+
+        worker.Surname = newSurname;
+        Assert.Equal(worker.Surname, newSurname);
+    }
+}
