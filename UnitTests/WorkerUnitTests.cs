006ea8f5d8afa0a0e21dd5c88dc7936ffa4bfad4
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-01 22:31:50 +0200

    Test worker constructor

--- UnitTests/WorkerUnitTests.cs
+++ UnitTests/WorkerUnitTests.cs
@@ -24,4 +24,31 @@ public class WorkerUnitTests
         worker.Surname = newSurname;
         Assert.Equal(worker.Surname, newSurname);
     }
+
+    [Fact]
+    public void TestConstructorWithoutContract()
+    {
+        Worker worker = new Worker("Name", "Surname");
+
+        Assert.Equal("Name", worker.Name);
+        Assert.Equal("Surname", worker.Surname);
+        Assert.False(worker.HasContract);
+    }
+
+    [Fact]
+    public void TestConstructorWithContract() {
+        Worker worker = new Worker("Name", "Surname", new Contract()
+        {
+            ContractType = ContractType.Freelance,
+            MaxDailyHours = 8,
+            MinDailyHours = 6
+        });
+
+        Assert.Equal("Name", worker.Name);
+        Assert.Equal("Surname", worker.Surname);
+        Assert.True(worker.HasContract);
+        Assert.Equal(ContractType.Freelance, worker.Contract?.ContractType);
+        Assert.Equal(8, worker.Contract?.MaxDailyHours);
+        Assert.Equal(6, worker.Contract?.MinDailyHours);
+    }
 }
