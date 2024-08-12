e924b86f9543de99d25033c3522c4d527d77b243
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-12 22:38:19 +0200

    Test WorkerAvailability default constructor

---
+++ UnitTests/WorkerAvailabilityUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkersTurnScheduler.Domain;
+
+namespace UnitTests
+{
+    public class WorkerAvailabilityUnitTests
+    {
+        [Fact]
+        void TestDefaultConstructor()
+        {
+            WorkerAvailability workerAvailability = new WorkerAvailability();
+
+            Assert.NotNull(workerAvailability);
+            Assert.True(workerAvailability.AvailabilitySlots.Count() == 0);
+            Assert.True(workerAvailability.Worker != null);
+        }
+    }
+}
