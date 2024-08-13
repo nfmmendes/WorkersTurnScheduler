b8b4da696422ab68d107f78c1e3e993dddcf5db1
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-13 23:35:57 +0200

    Add a Create page to Contract folder

---
+++ WorkersTurnScheduler/Pages/SchedulerArea/Contract/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkersTurnScheduler.Model;
+using WorkersTurnScheduler.Services;
+
+namespace WorkersTurnScheduler.Pages.SchedulerArea.Contract
+{
+    /// <summary>
+    /// Class <c>CreateModel</c> is a page model class that
+    /// holds that and methods to render the contract create page. 
+    /// </summary>
+    public class CreateModel : PageModel
+    {
+        /// <value>
+        /// Contract's repository.
+        /// </value>
+        private IContractRepository _repository;
+
+        /// <value>
+        /// Hold the contract data to be inserted in the repository. 
+        /// </value>
+        [BindProperty]
+        public Domain.Contract Contract {  get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="repository">Contract repository, injected as a dependence.</param>
+        public CreateModel(IContractRepository repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// Page get function.
+        /// </summary>
+        /// <returns>The page view</returns>
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
